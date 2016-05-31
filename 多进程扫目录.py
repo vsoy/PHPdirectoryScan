46186e9fa6d772a2677b2cb053341b2a7ec20c82
vsoy <754411721@qq.com>
2016-05-31 17:00:43 +0800

    Update 多进程扫目录.py

--- 多进程扫目录.py
+++ 多进程扫目录.py
@@ -12,7 +12,7 @@ import multiprocessing
 
 
 scanDict = './dict/PHP.txt'  # 字典
-proNum = 20 #进程数
+proNum = 20  # 进程数
 headers = {  # HTTP 头设置
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 }
@@ -23,21 +23,30 @@ try:
     check = urlparse.urlparse(url)
     if check.scheme == 'http':
         url = check.netloc
+    else:
+        raise     
 except Exception, e:
-    print 'Usage: python phpscan.py www.baidu.com'
+    print 'Usage: python phpscan.py http://www.baidu.com'
     exit()
-
 
 def scan(payload):
     try:
         taskurl = 'http://%s%s' % (url, payload)
         taskurl = taskurl.strip()
-        task = requests.get(taskurl, headers=headers,timeout=5)
-        print "%s:%s" % (taskurl,str(task.status_code))
+        task = requests.get(taskurl, headers=headers, timeout=5)
+        print "%s:%s" % (taskurl, str(task.status_code))
         if task.status_code == 200:
             return taskurl
     except Exception, err:
         print err
+
+def print_result(result):
+    for i in result:
+        try:
+            if i.get() != None:
+                print i.get()
+        except:
+            continue
 
 
 if __name__ == '__main__':
@@ -48,12 +57,7 @@ if __name__ == '__main__':
     pool.close()
     pool.join()
 
-    print '-' * 20
-    for i in result:
-        try:
-            if i.get() != None:
-                print i.get()
-        except:
-            continue
-    print '-' * 20
+    print '-' * 50
+    print_result(result)
+    print '-' * 50
     print 'End!'
